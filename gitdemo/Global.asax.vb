763121abe800a0cc614ea3525186052fcdd958be
unknown <1057823733@qq.com>
2014-01-08 09:56:00 +0800

    adc

---
+++ gitdemo/Global.asax.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Web.Optimization
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+        ' 应用程序启动时激发
+        BundleConfig.RegisterBundles(BundleTable.Bundles)
+        AuthConfig.RegisterOpenAuth()
+    End Sub
+
+    Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
+        ' 每个请求开始时激发
+    End Sub
+
+    Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
+        ' 尝试验证用户身份时激发
+    End Sub
+
+    Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+        ' 发生错误时激发
+    End Sub
+
+    Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
+        ' 应用程序结束时激发
+    End Sub
+End Class
